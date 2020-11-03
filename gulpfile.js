9cec45fcc22b9cda1e7e82c36e2422c550c95926
Kinkandju <ferretsphilosophy@gmail.com>
2020-11-03 15:42:32 +0300

    Добавляет разметку сайта и некоторые изображения в формате webp

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/icons/icon_*.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
